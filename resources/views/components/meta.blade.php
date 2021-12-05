ae6abae0becc72d027ce261dd8e943784a545a5d
Khurshidbek <kabilovhurshid5@gmail.com>
2021-12-05 13:56:53 +0500

    meta desc changed

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -1,7 +1,6 @@
 <meta charset="utf-8" />
 <title>Ancor</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-<meta content="Coderthemes" name="author" />
+<meta content="AntiCorruption Software for Uzbeksitan." name="description" />
 <!-- App favicon -->
 <link rel="shortcut icon" href="assets/images/favicon.ico">
